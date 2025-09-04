daa69fb4c92b5e63ba83e036ef7c9ae13b59c5a1
Anna Zadiraka <annazadiraka@gmail.com>
2025-09-04 18:43:39 +0300

    add nav menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
 // MENU
-const btnMenu = document.querySelector('.btn__menu');
+const btnMenu = document.getElementById('burger');
+const nav = document.getElementById('nav')
 let isOpenMenu = false;
 
 btnMenu.addEventListener('click', () => {
-    isOpenMenu = !isOpenMenu;
-    console.log('MENU', isOpenMenu);
+    if (isOpenMenu) {
+        btnMenu.classList.remove('open')
+        nav.classList.remove('open')
+    } else {
+        btnMenu.classList.add('open')
+        nav.classList.add('open')
+    }
+
+    isOpenMenu = !isOpenMenu;    
+    //console.log('MENU', isOpenMenu);
 })
+
+//Смена шапок при скролле
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const headerWrappers = document.querySelectorAll('.header__wrapper');
+ 
+    const currentScrollY = window.scrollY;
+    
+  
+    if (currentScrollY > lastScrollY && currentScrollY > 100) {
+    
+        headerWrappers[0].classList.add('header__wrapper--hidden');
+        headerWrappers[1].classList.remove('header__wrapper--hidden');
+    } else {
+        
+        headerWrappers[0].classList.remove('header__wrapper--hidden');
+        headerWrappers[1].classList.add('header__wrapper--hidden');
+    }
+
+    lastScrollY = currentScrollY;
+});
